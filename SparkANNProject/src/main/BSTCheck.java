e4c55be15e582b2af1682e8b3a150c65a9606337
Aavash Bhandari <aavashbhandari@gmail.com>
2023-01-27 13:47:26 +0900

    Test Code

--- SparkANNProject/src/main/BSTCheck.java
+++ SparkANNProject/src/main/BSTCheck.java
@@ -5,8 +5,6 @@ import algorithm.BinarySearchTree.TreeNode;
 
 import java.util.List;
 
-
-
 public class BSTCheck {
 	public static void main(String[] args) {
 		int[] array = { 5, 3, 7, 1, 4, 6, 8, 33, 67, 90, 76, 87, 99, 132, 145, 890 };
@@ -26,6 +24,8 @@ public class BSTCheck {
 		if (node != null) {
 			System.out.println(node.getData());
 		}
+
+		System.out.println("The bst is printing");
 
 //		RTree<Integer, Point> tree = RTree.create();
 //
